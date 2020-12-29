1918f324037c397271f3126e7e7956eaab95b11b
Hector Churata <hec000ale@gmail.com>
2020-12-29 12:42:51 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // inicializar variables
 var app = express();
+
+// body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la base de carousel-indicators
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res )=>{
@@ -13,14 +24,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res )
 })
 
 // Rutas
-app.get('/', (req, res, next ) =>{
-
-  res.status(200).json({
-    ok: true,
-    mensaje: 'Peticion realizada correctamente'
-  })
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 app.listen(3000,() =>{
